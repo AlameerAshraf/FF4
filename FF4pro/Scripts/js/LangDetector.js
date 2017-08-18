95abab4d7e72e5a1ca4fa11e14739e6124400e65
Alameer Ashraf Hassan <alamiir.ashraf@gmail.com>
2017-08-18 03:16:27 +0200

    ServiceRequest Translation

---
+++ FF4pro/Scripts/js/LangDetector.js
@@ -0,0 +1,12 @@
+$(function () {
+    $("#langdete").click(function () {
+        var CurrentLang = $(this).attr("langdete");
+        if  ($(this).attr("langdete") == "en") {
+            window.location.replace("http://localhost:1337/ServiceRequest?lang=ar");
+        }
+        else if (CurrentLang = $(this).attr("langdete") == "ar") {
+            window.location.replace("http://localhost:1337/ServiceRequest?lang=en");
+        }
+    });
+
+});
